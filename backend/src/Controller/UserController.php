5519e4fb18daa99ab5b8cd6f58238ea061c66f93
Pavel Fanaskov <paul-fanaskov@yandex-team.ru>
2021-02-17 20:31:49 +0300

    create user controller

---
+++ backend/src/Controller/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function users(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+    /**
+     * @Route("/user/{id}", name="user")
+     */
+    public function user($id): Response
+    {
+        return $this->json([
+            'id' => $id
+        ]);
+    }
+}
