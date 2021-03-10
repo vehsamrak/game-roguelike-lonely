c26ecc4c93c0c45eca3c5c6cc6211561f703c115
Pavel Fanaskov <paul-fanaskov@yandex-team.ru>
2021-03-10 13:21:33 +0300

    1-create-user-entity

---
+++ backend/src/Entity/User.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=190)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=190)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $hp;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $posX;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $posY;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $posZ;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $token;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getHp(): ?float
+    {
+        return $this->hp;
+    }
+
+    public function setHp(float $hp): self
+    {
+        $this->hp = $hp;
+
+        return $this;
+    }
+
+    public function getPosX(): ?int
+    {
+        return $this->posX;
+    }
+
+    public function setPosX(int $posX): self
+    {
+        $this->posX = $posX;
+
+        return $this;
+    }
+
+    public function getPosY(): ?int
+    {
+        return $this->posY;
+    }
+
+    public function setPosY(int $posY): self
+    {
+        $this->posY = $posY;
+
+        return $this;
+    }
+
+    public function getPosZ(): ?int
+    {
+        return $this->posZ;
+    }
+
+    public function setPosZ(int $posZ): self
+    {
+        $this->posZ = $posZ;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+}
